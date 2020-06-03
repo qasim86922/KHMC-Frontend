482f0408bd022b9b8bc6e5d778649136c0dd300b
qasim <qasim86922@gmail.com>
2020-06-04 00:49:15 +0500

    Build added

---
+++ build/precache-manifest.6bce592c926ceb0438593d41b9d0e926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78c552a7e24f2084111a7149d410c52",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "cb32fc0641f33b8aacf6",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "870cc191b69d47b72508",
+    "url": "/material-dashboard-react/static/css/main.c1268cae.chunk.css"
+  },
+  {
+    "revision": "cb32fc0641f33b8aacf6",
+    "url": "/material-dashboard-react/static/js/2.70d372b4.chunk.js"
+  },
+  {
+    "revision": "870cc191b69d47b72508",
+    "url": "/material-dashboard-react/static/js/main.24c61328.chunk.js"
+  },
+  {
+    "revision": "1e2d2b9757d409171d00",
+    "url": "/material-dashboard-react/static/js/runtime~main.1c8291c9.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  }
+]);
